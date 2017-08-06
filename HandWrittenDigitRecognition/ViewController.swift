68cd5c1679dfbbb4ee1a99544fc8d9d62408e956
Ashis Laha <ashis.laha@olacabs.com>
2017-08-06 13:02:08 +0530

    Initial Commit

---
+++ HandWrittenDigitRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandWrittenDigitRecognition
+//
+//  Created by Ashis Laha on 06/08/17.
+//  Copyright © 2017 Ashis Laha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
